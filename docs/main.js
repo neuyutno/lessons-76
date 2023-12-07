05eb7446169be6ce1fdae3d68879aa3c676618a1
neuyutno <148278153+neuyutno@users.noreply.github.com>
2023-12-07 21:19:10 +0300

    Add files via upload

---
+++ docs/main.js
@@ -0,0 +1,15 @@
+const userName = 'Anatoly'
+const surName = 'Nazarov'
+const patronymic = 'Evgenievich'
+const age = '27'
+const country = 'Russian Federation'
+
+console.log(userName)
+console.log(surName)
+console.log(patronymic)
+console.log(age)
+console.log(country)
+
+
+// Разница между переменной let и const заключается в том,
+// let позволяет изменить в любой момент, а const - нет.
